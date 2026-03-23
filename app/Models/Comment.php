fd3302374ef557382fc84d8ed2966f17ac606994
piyush rajput <piyush123kdif@gmail.com>
2026-03-23 19:33:06 +0530

    jagods reach counts

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'social_mysql'; // 🔥 VERY IMPORTANT
+
+    protected $fillable = [
+        'user_id',
+        'link_id',
+        'comment'
+    ];
+
+    // (optional) user relation future ke liye
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
